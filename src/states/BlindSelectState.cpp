2b5a2158e25964455baef1a676e09fe0187c2389
Aaron Thakoordeen <aaronthakoordeen@gmail.com>
2026-03-25 15:09:32 -0400

    feat: add BlindSelectState stub and wire into CMake

---
+++ src/states/BlindSelectState.cpp
@@ -0,0 +1,46 @@
+#include "BlindSelectState.h"
+
+#include "GameplayState.h"
+#include "../core/Application.h"
+#include "../core/StateMachine.h"
+#include "../core/TextRenderer.h"
+
+#ifdef N3DS
+#include <3ds.h>
+#include <citro2d.h>
+#else
+#include <SDL.h>
+#endif
+
+#include <string>
+#include <utility>
+
+BlindSelectState::BlindSelectState(StateMachine* machine, std::shared_ptr<RunState> runState)
+    : m_runState(std::move(runState)) {
+    m_stateMachine = machine;
+}
+
+void BlindSelectState::enter() {
+    m_cursorIndex = 0;
+    m_inputDelay = 0.3f;
+}
+
+void BlindSelectState::exit() {}
+
+bool BlindSelectState::canSkip() const {
+    return m_runState->nextBlindStage() != BlindStage::Boss;
+}
+
+void BlindSelectState::confirmSelection() {}
+
+void BlindSelectState::update(float dt) {
+    if (m_inputDelay > 0.0f) {
+        m_inputDelay -= dt;
+    }
+}
+
+void BlindSelectState::handleInput() {}
+
+void BlindSelectState::renderTopScreen(Application* /*app*/) {}
+
+void BlindSelectState::renderBottomScreen(Application* /*app*/) {}
---
+++ src/states/BlindSelectState.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../core/State.h"
+#include "../game/RunState.h"
+#include <memory>
+
+class BlindSelectState : public State {
+public:
+    BlindSelectState(StateMachine* machine, std::shared_ptr<RunState> runState);
+    ~BlindSelectState() override = default;
+
+    void enter() override;
+    void exit() override;
+    void handleInput() override;
+    void update(float dt) override;
+    void renderTopScreen(Application* app) override;
+    void renderBottomScreen(Application* app) override;
+
+private:
+    void confirmSelection();
+    bool canSkip() const;
+
+    std::shared_ptr<RunState> m_runState;
+    int m_cursorIndex = 0;
+    float m_inputDelay = 0.3f;
+};
